57bf38ba470e31290cdd82aa7baad21fba4a27f3
Vincent <vincentonor8lite@gmail.com>
2018-09-15 23:38:20 +0200

    Added: Main menu, 3 seconds delay before game starts

---
+++ RFTS/Assets/Scripts/DelayedStart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine("StartDelay");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator StartDelay()
+    {
+        Time.timeScale = 0;
+        float pauseTime = Time.realtimeSinceStartup + 3f;
+        while (Time.realtimeSinceStartup < pauseTime)
+            yield return 0;
+        Time.timeScale = 1;
+
+    }
+
+}
