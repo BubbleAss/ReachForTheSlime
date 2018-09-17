08a5240ec4315de776f5c898f345fd0effb55ec2
BubbleAss <42513623+BubbleAss@users.noreply.github.com>
2018-09-17 10:23:21 +0200

    Using SceneManager instead of Application.LoadLevel

--- RFTS/Assets/Scripts/MainMenu.cs
+++ RFTS/Assets/Scripts/MainMenu.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
     public void LoadGame()
     {
-        Application.LoadLevel("Game");
+        SceneManager.LoadScene("Game")
     }
 }
